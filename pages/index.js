d8071014dbd5acb8db7d91f3e01945dbe92456a7
preska <wolfwar@outlook.com>
2023-01-14 10:11:57 +0100

    feed component with header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import Sidebar from '../components/Sidebar';
+import Feed from '../components/Feed';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function Home() {
         <Sidebar />
 
         {/* Feed */}
+        <Feed />
 
         {/* Widgets */}
 
